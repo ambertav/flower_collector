56517476a97877f0352dce6bd7442c2d883f6a23
Amber Taveras <taveras.amber@gmail.com>
2023-04-10 12:37:52 -0400

    adds index, create view and path

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('flowers/', views.FlowerList.as_view(), name='flower_list'),
+    path('flowers/create/', views.FlowerCreate.as_view(), name='flower_create')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+
+from .models import Flower
 
 # Create your views here.
 
 def home(request):
-  return HttpResponse('<h1>Hello world</h1>')
+    return render(request, 'home.html')
+
+class FlowerList (ListView) :
+    model = Flower
+    template_name = 'flowers/flower_list.html'
+
+class FlowerCreate (CreateView) :
+    model = Flower
+    fields = '__all__'
+    template_name = 'flowers/flower_form.html'
