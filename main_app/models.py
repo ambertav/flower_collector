22d657be817a6aabba2ab4c28e60016be8f777a0
Amber Taveras <taveras.amber@gmail.com>
2023-04-10 12:32:04 -0400

    adds flower model and migrates

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Flower (models.Model) :
+
+    STAGES = (
+        ('S', 'Seed'),
+        ('G', 'Germination'),
+        ('F', 'Flowering'),
+        ('P', 'Pollination'),
+    )
+
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    stage = models.CharField(max_length=1, choices=STAGES, default=STAGES[0][0])
+
+    def __str__ (self) :
+        return self.name
