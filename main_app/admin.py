be0e026a4dd8d8e1228a0d5a751a9643ad6e7757
Amber Taveras <taveras.amber@gmail.com>
2023-05-08 22:21:29 -0400

    adds photo model to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Flower, Watering, Pollinator
+from .models import Flower, Watering, Pollinator, Photo
 
 # Register your models here.
 
-admin.site.register([Flower, Watering, Pollinator])
+admin.site.register([Flower, Watering, Pollinator, Photo])
